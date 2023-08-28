163925345e6430bef2f023f262afb9580301ccdb
Benjamin <benjamin.alazet@gmail.com>
2023-08-28 11:20:27 +0200

    Add Iterable

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Type-annotated function element_length"""
-from typing import Tuple, Sequence, List, Tuple
+from typing import Tuple, Sequence, List, Tuple, Iterable
 
 
-def element_length(lst: Sequence) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Takes a Sequence as argument and returns a
     list of tuple of Sequence and int"""
     return [(i, len(i)) for i in lst]
