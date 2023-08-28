a32203b3c2b3e1d157b3c817a596a62674552711
Benjamin <benjamin.alazet@gmail.com>
2023-08-28 10:03:31 +0200

    Type-annotated function concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Type-annotated function concat"""
+
+
+def concat(str1 :str, str2: str) -> str:
+    """Takes two strings as arguments and returns a concatenated string"""
+    concat_str = str1 + str2
+    return concat_str
