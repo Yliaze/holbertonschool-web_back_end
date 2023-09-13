bb91586a02cca51e43d3c6ac0187898cad548df6
Benjamin <benjamin.alazet@gmail.com>
2023-09-13 15:08:02 +0200

    try something

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,11 +1,10 @@
-import ClassRoom from './0-classroom';
+import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
-    const objClass = [
+    const objclass = [
         new ClassRoom(19),
         new ClassRoom(20),
         new ClassRoom(34)
     ];
-
-    return objClass
+    return objclass
 }
