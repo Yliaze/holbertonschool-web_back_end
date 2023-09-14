d330a585a5de6fdd0308165d42675bb159fcce62
Benjamin <benjamin.alazet@gmail.com>
2023-09-14 14:23:55 +0200

    Modify symbols

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,23 +1,15 @@
 export default class Car {
-    constructor(brand, motor, color) {
-      const brandSymbol = Symbol('brand');
-      const motorSymbol = Symbol('motor');
-      const colorSymbol = Symbol('color');
-  
-      this._brand = brand;
-      this._motor = motor;
-      this._color = color;
-  
-      // Make the symbols private
-      Object.freeze(this);
-    }
-  
-    cloneCar() {
-        return new this.constructor(
-          this[Symbol.for('brand')],
-          this[Symbol.for('motor')],
-          this[Symbol.for('color')]
-        );
-      }
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
-  
+
+  cloneCar() {
+    return new this.constructor(
+      this[Symbol.for('brand')],
+      this[Symbol.for('motor')],
+      this[Symbol.for('color')],
+    );
+  }
+}
