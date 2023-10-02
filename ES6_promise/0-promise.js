5e984bd4844217051bf302cc40ffb170ecd366fa
Benjamin <benjamin.alazet@gmail.com>
2023-10-02 10:22:50 +0200

    Return a Promise using this prototype function getResponseFromAPI()

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+export default function getResponseFromAPI () {
+  return new Promise((resolve, reject) => {
+    const Success = true;
+    if (Success) {
+      resolve('Promise resolved');
+    } else {
+      reject(new Error('Promise rejected'));
+    }
+  });
+}
