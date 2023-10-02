740c181f118e5b4052588931a8ad83a9a29fbe15
Benjamin <benjamin.alazet@gmail.com>
2023-10-02 14:21:38 +0200

    Try to add return and change consol.error by console.log

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,11 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const allPromise = Promise.all([uploadPhoto(), createUser()]);
-  allPromise.then((results) => {
-    console.log(results[0].body, results[1].firstName, results[1].lastName);
-  })
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      console.log(results[0].body, results[1].firstName, results[1].firstName);
+    })
     .catch(() => {
-      console.error('Signup system offline');
+      console.log('Signup system offline');
     });
 }
