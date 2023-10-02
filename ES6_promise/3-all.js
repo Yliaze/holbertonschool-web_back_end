6302e2b52ad647c76c9505794e596005492e9466
Benjamin <benjamin.alazet@gmail.com>
2023-10-02 14:24:17 +0200

    Add variables ${}

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
-      console.log(results[0].body, results[1].firstName, results[1].firstName);
+      console.log(`${results[0].body} ${results[1].firstName} ${results[1].firstName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
