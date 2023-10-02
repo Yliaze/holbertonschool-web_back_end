7de18656df045b2cac5cd3c9f2dfd8b2419ddbdd
Benjamin <benjamin.alazet@gmail.com>
2023-10-02 15:34:22 +0200

    Real one in comment............

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,9 +1,24 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
+// The one by Holberton dev :
+
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => {
-      console.log(results);
-    });
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([userPromise, photoPromise])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
+    })));
 }
+
+// Real One by normal dev :
+
+// export default function handleProfileSignup(firstName, lastName, fileName) {
+//   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+//     .then((results) => {
+//       console.log(results);
+//     });
+// }
