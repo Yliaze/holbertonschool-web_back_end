37bc709af068be105892d39df9ba569b1eca8e38
Benjamin <benjamin.alazet@gmail.com>
2023-10-02 15:24:20 +0200

    Add console.log(results)

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,6 +1,9 @@
-import signUpUser from "./4-user-promise" 
-import uploadPhoto from "./5-photo-reject"
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)])
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => {
+      console.log(results);
+    });
 }
