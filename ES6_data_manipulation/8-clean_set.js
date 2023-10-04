f44cca22c1d0c32eedd2fbea087e8789e7c562d8
Benjamin <benjamin.alazet@gmail.com>
2023-10-04 10:40:18 +0200

    Add typeof 'string' and endWith('-')

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,19 @@
 export default function cleanSet(set, startString) {
-  if (startString !== '') {
-    const transformArray = Array.from(set);
-    const cleanArray = transformArray.filter((obj) => obj.startsWith(startString));
+  if (typeof startString !== 'string') {
+    return '';
+  }
 
-    let result = '';
-    for (const value of cleanArray) {
+  let result = '';
+  for (const value of set) {
+    if (value.startsWith(startString)) {
       result += `${value.slice(startString.length)}-`;
     }
-    result = result.slice(0, -1);
-    return result;
   }
-  return '';
+
+  result = result.slice(0, -1); // Enlève le dernier "-"
+  if (result.endsWith('-')) {
+    result = result.slice(0, -1); // Enlève encore le dernier "-"
+  }
+
+  return result;
 }
